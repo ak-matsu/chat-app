6b57f288087f4767e0170d99c645a9fe930f636d
ak-matsu <sandbox.matsu@gmail.com>
2021-02-22 20:54:21 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  #ログインしていないユーザーをログインページの画面に促すことができる。
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #nameカラムを追加、nameキーの内容の保存を.permitメソッドで許可している。
+  end
+
 end
