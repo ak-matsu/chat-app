7019d9b92ba8d4821c92a23572cdcc0cffa69b7f
ak-matsu <sandbox.matsu@gmail.com>
2021-02-25 21:59:02 +0900

    roomモデルのテスト終了

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  describe '#create'  do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it 'nameの値が存在すれば登録できること' do
+      expect(@room).to be_valid
+    end
+
+    it 'nameが空では登録できないこと' do
+      @room.name = ''
+      @room.valid?
+      #@room.errors
+      #@room.errors.full_messages
+      expect(@room.errors.full_messages).to include("Name can't be blank")
+
+
+    end
+  end
 end
