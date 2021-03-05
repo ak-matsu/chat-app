8a1b47f4516b2dd879ea0524af0d2c26d3b8adb3
ak-matsu <sandbox.matsu@gmail.com>
2021-03-06 00:56:57 +0900

    コメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
+    #devise_parameter_sanitizerとはdeviseでユーザー登録をする場合に使用でき、
+    # 「特定のカラムを許容する」メソッド
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     #nameカラムを追加、nameキーの内容の保存を.permitメソッドで許可している。
   end
