8770ff98d9ad9aff8bf47b7a34b99b208d595c36
ak-matsu <sandbox.matsu@gmail.com>
2021-03-06 18:24:58 +0900

    コメントアウト

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   has_many :room_users, dependent: :destroy
+  #throughオプションはモデルに多対多の関連を定義するときに使う。
   has_many :users, through: :room_users
   has_many :messages, dependent: :destroy
 
